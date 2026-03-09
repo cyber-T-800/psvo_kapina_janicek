ad331731051b4bcf01144c716f10d82d66d06097
Tadeáš Kapina <tadeas.kapina4@gmail.com>
2026-03-09 16:23:09 +0100

    pridane trackbar testy

---
+++ zadanie2/trackbar_try.py
@@ -0,0 +1,21 @@
+import cv2
+
+def nothing(x):
+    pass
+
+
+cv2.namedWindow('image')
+
+# create trackbars for color change
+cv2.createTrackbar('R', 'image', 0, 255, nothing)
+cv2.createTrackbar('G', 'image', 0, 255, nothing)
+cv2.createTrackbar('B', 'image', 0, 255, nothing)
+
+while (1):
+    cv2.waitKey(1)
+
+    # get current positions of four trackbars
+    r = cv2.getTrackbarPos('R', 'image')
+    g = cv2.getTrackbarPos('G', 'image')
+    b = cv2.getTrackbarPos('B', 'image')
+
