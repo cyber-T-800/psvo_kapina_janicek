152ff042e2972f52b9c67f14828937821d1d5abb
Tadeáš Kapina <tadeas.kapina4@gmail.com>
2026-03-16 16:05:51 +0100

    3. zadanie, pridany gaussov filter

--- zadanie3canny/canny.py
+++ zadanie3canny/canny.py
@@ -1,8 +1,19 @@
+from random import gauss
+
 import numpy as np
 import cv2 as cv
-import matplotlib.pyplot as plt
 
-image = cv.imread('messi.jpg', cv.IMREAD_GRAYSCALE)
+image = cv.imread('messi.png', cv.IMREAD_GRAYSCALE)
+
+gauss = np.array([
+    [0.002969, 0.013306, 0.021938, 0.013306, 0.002969],
+    [0.013306, 0.059634, 0.09832,  0.059634, 0.013306],
+    [0.021938, 0.09832,  0.1621,   0.09832,  0.021938],
+    [0.013306, 0.059634, 0.09832,  0.059634, 0.013306],
+    [0.002969, 0.013306, 0.021938, 0.013306, 0.002969]
+])
+
+print(gauss)
 
 # todo noise reduction by gaussian 5x5 filter
 
