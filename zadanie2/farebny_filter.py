61b92ad9b403a404f68880a8e5a4be7dada8a33a
Tadeáš Kapina <tadeas.kapina4@gmail.com>
2026-03-16 15:13:31 +0100

    zmena farby vo farebnom filtry

--- zadanie2/farebny_filter.py
+++ zadanie2/farebny_filter.py
@@ -13,17 +13,20 @@ while True:
 
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    # lower_blue = np.array([110,50,50])
+    # upper_blue = np.array([130,255,255])
+    #
+    # lower_white = np.array([0,0,235])
+    # upper_white = np.array([255,255,255])
 
-    lower_white = np.array([0,0,235])
-    upper_white = np.array([255,255,255])
+    lower_red = np.array([0, 100, 100])
+    upper_red = np.array([20, 255, 255])
 
     #mask = cv.inRange(hsv, lower_blue, upper_blue)
-    mask = cv.inRange(hsv, lower_white, upper_white)
+    mask = cv.inRange(hsv, lower_red, upper_red )
 
     res = cv.bitwise_and(img, img)
-    res[mask>0]=(0,0,255, 255)
+    res[mask>0]=(255,0,0, 255)
 
     cv.imshow('mask',mask)
     cv.imshow('image', img)
